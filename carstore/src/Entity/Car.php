5fe0cd4f6b0bf543f43acd0d57f246820a9debb1
Eduardo PINA <e.pina@adlperformance.fr>
2023-06-22 01:01:42 +0200

    * Set up initial project config
    * Add packages
    
    * Add entity Car and CarCategory
    
    * Kickoff !

---
+++ carstore/src/Entity/Car.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CarRepository::class)]
+class Car
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: '0')]
+    private ?string $cost = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $nbSeats = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $nbDoors = null;
+
+    #[ORM\ManyToOne(inversedBy: 'cars')]
+    private ?CarCategory $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCost(): ?string
+    {
+        return $this->cost;
+    }
+
+    public function setCost(string $cost): static
+    {
+        $this->cost = $cost;
+
+        return $this;
+    }
+
+    public function getNbSeats(): ?int
+    {
+        return $this->nbSeats;
+    }
+
+    public function setNbSeats(int $nbSeats): static
+    {
+        $this->nbSeats = $nbSeats;
+
+        return $this;
+    }
+
+    public function getNbDoors(): ?int
+    {
+        return $this->nbDoors;
+    }
+
+    public function setNbDoors(int $nbDoors): static
+    {
+        $this->nbDoors = $nbDoors;
+
+        return $this;
+    }
+
+    public function getCategory(): ?CarCategory
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?CarCategory $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
